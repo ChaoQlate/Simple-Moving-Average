69d63acb1a5bfc9cb203fe5d17aeecd1fb684610
Kevin Zhang <z5122525@student.unsw.edu.au>
2019-01-22 23:24:19 +1100

    Initialised files

---
+++ dateaxis.py
@@ -0,0 +1,73 @@
+import pyqtgraph as pg
+from enum import Enum
+import time
+
+class DateAxis(pg.AxisItem):
+
+    def __init__(self, orientation, pen=None, linkView=None, parent=None, maxTickLength=-5, showValues=True):
+        super(DateAxis, self).__init__(orientation, pen=None, linkView=None, parent=None, maxTickLength=-5, showValues=True)
+
+        #below are used in calculating the ticks
+        #required to store and evaluate the order of ticks
+        self.dateMode = None
+        self.dateEnum = Enum("dateEnum", "hour day month year")
+        self.dateFmtDict = {
+            1 : "%H:%M:%S",
+            2 : "%d",
+            3 : "%b",
+            4 : "%Y"
+        }
+
+    #overides the definition in AxisItem
+    #calculates the exact spacing of axis ticks on key time measures
+    #years, months, days, hours
+    #returns a list of tuples
+    #   [   (major tick spacing, offset),
+    #       (minor tick spacing, offset),
+    #       (subminor tick spacing, offset),
+    #       ...
+    #   ]
+    def tickSpacing(self, minVal, maxVal, size):
+        # First check for override tick spacing
+        if self._tickSpacing is not None:
+            return self._tickSpacing
+        #returns the 2 max corresponding spacing of time measurements
+        levels = []
+        dif = abs(maxVal - minVal)
+
+        if dif >= 365 * 24 * 60 * 60:
+            levels.append((365 * 24 * 60 * 60, 0))
+            self.dateMode = self.dateEnum.year.value
+
+        if dif >= 30 * 24 * 60 * 60:
+            levels.append((30 * 24 * 60 * 60, 0))
+            self.dateMode = self.dateEnum.month.value if self.dateEnum.month.value > self.dateMode else self.dateMode
+
+        if dif >= 24 * 60 * 60:
+            levels.append((24 * 60 * 60, 0))
+            self.dateMode = self.dateEnum.day.value if self.dateEnum.day.value > self.dateMode else self.dateMode
+
+        if dif >= 60 * 60:
+            levels.append((60 * 60, 0))
+            self.dateMode = self.dateEnum.hour.value if self.dateEnum.hour.value > self.dateMode else self.dateMode
+
+        while (len(levels) > 2):
+            levels.pop()
+        
+        return levels
+
+    #overrides the definition in AxisItem
+    #provides a list of names for specified ticks
+    def tickStrings(self, values, scale, spacing):
+        strns = []
+
+        if values == []:
+            return []
+        for val in values:
+            strns.append(time.strftime(self.dateFmtDict[self.dateMode], time.localtime(val)))
+        self.dateMode -= 1
+
+        label = "Time " + time.strftime("%d-%b-%y", time.localtime(min(values))) + " to" + time.strftime("%d-%b-%y",time.localtime(max(values)))
+        self.setLabel(text=label)
+
+        return strns
---
+++ sma.py
@@ -0,0 +1,62 @@
+import pyqtgraph as pg
+import numpy as np
+import dateaxis as da
+import queue
+import time
+import csv
+
+class SimpleMovingAverage():
+    def __init__(self, period=50):
+        self.period = period
+        self.queue = queue.Queue(period + 1)
+    
+    def update(self, data):
+        if type(data) != type([]):
+            data = [data]
+        for v in data:
+            if type(v) != int and type(v) != float:
+                raise TypeError("in sma.update value(s) for data parameter must be int or float")
+            try:
+                self.queue.put(v, block=False)
+            except queue.Full:
+                self.queue.get(block=False)
+                self.queue.put(v, block=False)
+    
+    def getAverage(self):
+        total = 0
+        count = 0
+        for elem in list(self.queue.queue):
+            total += elem
+            count += 1
+
+        return float(total) / count
+
+win = pg.GraphicsWindow(title="Simple Moving Average")
+win.resize(1000,600)
+plot = win.addPlot(title="TSLA", axisItems={"bottom" : da.DateAxis(orientation="bottom")})
+
+sma = SimpleMovingAverage(50)
+smaVals = []
+
+xVals = []
+yVals = []
+with open('TSLA.csv', newline='') as f:
+    next(f)
+    reader = csv.reader(f)
+    for row in reader:
+        xVals.append(time.mktime(time.strptime(row[0], "%Y-%m-%d")))
+        yVals.append(float(row[2]))
+
+        sma.update(float(row[2]))
+        smaVals.append(sma.getAverage())
+
+
+
+plot.plot(x=xVals, y=yVals, pen='b')
+plot.plot(x=xVals, y=smaVals, pen='r')
+
+
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        pg.QtGui.QApplication.exec_()
